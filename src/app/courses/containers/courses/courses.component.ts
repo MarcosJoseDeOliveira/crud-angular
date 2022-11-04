e9ac6bcd70fe75617e945e5fe8eb3291ae57091c
Marcos José de Oliveira <marcosjose000@gmail.com>
2022-11-04 10:36:53 -0300

    aula 25 concluída, refatorações

--- src/app/courses/containers/courses/courses.component.ts
+++ src/app/courses/containers/courses/courses.component.ts
@@ -1,12 +1,12 @@
-import { ErrorDialogComponent } from './../../shared/components/error-dialog/error-dialog.component';
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
-import { CoursesService } from '../services/courses.service';
-import { Course } from './../model/course';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ErrorDialogComponent } from '../../../shared/components/error-dialog/error-dialog.component';
+import { Course } from '../../model/course';
+import { CoursesService } from '../../services/courses.service';
 
 @Component({
   selector: 'app-courses',
